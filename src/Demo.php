5c43e06a0f5a3b6ea9f384d4c5efb8ac17303859
xiaohe4966 <496631085@qq.com>
2022-03-26 15:20:25 +0800

    test

---
+++ src/Demo.php
@@ -0,0 +1,10 @@
+<?php
+namespace XiaoHe\Hello;
+ 
+class Demo
+{
+    public function p()
+    {
+        echo '我是一个测试包'.'this is composer'.date('Y-m-d H:i:s').PHP_EOL.'496631085@qq.com', PHP_EOL;
+    }
+}
